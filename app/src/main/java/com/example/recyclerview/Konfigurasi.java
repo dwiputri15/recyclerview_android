5130f5cbad595d513eebd36129052de001441f7b
dwiputri15 <100397747+dwiputri15@users.noreply.github.com>
2022-06-21 20:51:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Konfigurasi.java
@@ -0,0 +1,14 @@
+package com.example.recyclerview;
+
+public class Konfigurasi {
+
+    public static final  String URL_GET_DATA = "http://192.168.1.25/crud/tampil_data.php";
+
+    //json tags
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public  static  final String TAG_NIM = "NIM";
+
+    //id
+    public static final String EMP_ID = "emp_id";
+}
